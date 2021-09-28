8581828b59358759c706e7e44e9dc87272eb887d
Teri Jensen <teriannjensen@gmail.com>
2021-09-28 17:43:57 -0600

    addiing var and lists

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Teri-s-Friendly-Parakeet
+# Challenge-Three- Teri-s-Friendly-Parakeet
 
+## Purpose
+
+
+## Built with
+* HTML
+* CSS
+* JS
+
+## Website 
+https://tjen20.github.io/Teri-s-Friendly-Parakeet/
--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 // Assignment code here
+function generatePassword(){
+    var passwordLength = parseInt(prompt('How long do you want your password to be?'));
+    var lowercase = confirm('Do you want lowercase in your password?');
+    var uppercase = confirm('Do you want uppercase in your password?');
+    var numeric = confirm('Do you want to include numbers?');
+    var specialCharacters = confirm('Do you want to include special characters?');
+    
+    var lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz'
+    var uppercaseCharacters = 'ABCDEFGHIJKLMNOPQURSTUVWXYZ'
+    var numericCharacters = '1234567890'
+    var specialCharacterOptions = '!@#$%^&*()-+_='
+    var passwordCharacterOptions = ''
 
+    if (passwordLength < 8 || passwordLength >128) {
+        alert('Must be between 8 and 128 characters')
+    }
+
+    if (lowercase == true) {
+        passwordCharacterOptions = passwordCharacterOptions + lowercaseCharacters
+    }
+
+    if (uppercase == true) {
+        passwordCharacterOptions = passwordCharacterOptions + uppercaseCharacters
+    }
+
+    if (numeric == true) {
+        passwordCharacterOptions = passwordCharacterOptions + numericCharacters
+    }
+
+    if (specialCharacters == true) {
+        passwordCharacterOptions = passwordCharacterOptions + specialCharacterOptions
+    }
+
+    if (lowercase == false && uppercase == false && numericCharacters == false && specialCharacters == false){
+        alert('Must select at least one category')
+        return
+    }
+
+    console.log(passwordLength, lowercase, uppercase, numeric, specialCharacters, passwordCharacterOptions)
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
